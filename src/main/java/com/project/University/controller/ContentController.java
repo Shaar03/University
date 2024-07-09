a962b778dcabc9c2de0cca2a82d6db9279563338
Shaar.03 <ashaar409@gmail.com>
2024-07-09 11:55:09 +0300

    change paths + validate pageNo and pageSize using a function

--- src/main/java/com/project/University/controller/ContentController.java
+++ src/main/java/com/project/University/controller/ContentController.java
@@ -3,20 +3,18 @@ package com.project.University.controller;
 import com.project.University.entity.Course;
 import com.project.University.entity.Semester;
 import com.project.University.entity.Student;
+import com.project.University.repository.projection.StudentIP;
 import com.project.University.service.CourseService;
 import com.project.University.service.SemesterService;
 import com.project.University.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.web.PagedModel;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.Instant;
 import java.time.LocalDate;
 
 @RestController
-@RequestMapping(path = "api/view")
-@PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+@RequestMapping(path = "api")
 public class ContentController {
 
     @Autowired
@@ -36,14 +34,12 @@ public class ContentController {
     ){
         long totalRecords = studentService.countStudents();
 
-        pageSize = pageSize < 0 || pageSize > 5? 5: pageSize;
-        pageNo = pageNo < 0 || pageNo > totalRecords / pageSize? 0: pageNo;
+        int[] page = validatePage(pageNo, pageSize, totalRecords);
 
-        return new PagedModel<>(studentService.getStudents(age, studentName, courseName, pageNo, pageSize));
+        return new PagedModel<>(studentService.getStudents(age, studentName, courseName, page[0], page[1]));
     }
 
-
-    @GetMapping(path = "/courses")
+    @GetMapping(path = "/admin/view/courses")
     public PagedModel<Course> getCourses(
             @RequestParam(required = false) Integer creditHours,
             @RequestParam(required = false) Integer noOfStudents,
@@ -52,13 +48,12 @@ public class ContentController {
     ){
         long totalRecords = courseService.countCourses();
 
-        pageSize = pageSize < 0 || pageSize > 5? 5: pageSize;
-        pageNo = pageNo < 0 || pageNo > totalRecords / pageSize? 0: pageNo;
+        int[] page = validatePage(pageNo, pageSize, totalRecords);
 
-        return new PagedModel<>(courseService.getCourses(creditHours, noOfStudents, pageNo, pageSize));
+        return new PagedModel<>(courseService.getCourses(creditHours, noOfStudents, page[0], page[1]));
     }
 
-    @GetMapping(path = "/semesters")
+    @GetMapping(path = "/admin/view/semesters")
     public PagedModel<Semester> getSemesters(
             @RequestParam(required = false) LocalDate providedDate,
             @RequestParam(defaultValue = "0") int pageNo,
@@ -66,9 +61,16 @@ public class ContentController {
     ){
         long totalRecords = semesterService.countSemesters();
 
-        pageSize = pageSize < 0 || pageSize > 5? 5: pageSize;
-        pageNo = pageNo < 0 || pageNo > totalRecords / pageSize? 0: pageNo;
+        int[] page = validatePage(pageNo, pageSize, totalRecords);
 
-        return new PagedModel<>(semesterService.getSemesters(providedDate, pageNo, pageSize));
+        return new PagedModel<>(semesterService.getSemesters(providedDate, page[0], page[1]));
+    }
+
+    private int[] validatePage(int pageNo, int pageSize, long totalRecords){
+        int[] page = new int[2];
+        page[0] = pageNo < 0 || pageNo > totalRecords / pageSize? 0: pageNo;
+        page[1] = pageSize < 0 || pageSize > 5? 5: pageSize;
+
+        return page;
     }
 }

fc83c30a77cafc7ae892f15aa68eda570e37a064
Shaar.03 <ashaar409@gmail.com>
2024-07-17 12:07:35 +0300

    send test data through kafka template

--- src/main/java/com/project/University/UniversityApplication.java
+++ src/main/java/com/project/University/UniversityApplication.java
@@ -1,8 +1,11 @@
 package com.project.University;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 @EnableCaching
@@ -12,4 +15,11 @@ public class UniversityApplication {
 		SpringApplication.run(UniversityApplication.class, args);
 	}
 
+	@Bean
+	ApplicationRunner runner(KafkaTemplate<String, String> kafkaTemplate){
+		return args -> {
+			for(int i = 0; i < 100; i++)
+				kafkaTemplate.send("university", "hello uni " + i);
+		};
+	}
 }
